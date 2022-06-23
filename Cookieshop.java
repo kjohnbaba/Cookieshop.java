d15d19658b4ba20ee85a48926be4663cbf0f5b99
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2022-06-23 12:19:42 -0400

    Update Cookieshop.java

--- Cookieshop.java
+++ Cookieshop.java
@@ -1,17 +1,18 @@
 public class Store {
-  // instance fields
+
   String productType;
   int inventoryCount;
   double inventoryPrice;
   
-  // constructor method
+  // constructor 
   public Store(String product, int count, double price) {
     productType = product;
     inventoryCount = count;
     inventoryPrice = price;
   }
   
-  // main method
+  
+  
   public static void main(String[] args) {
     Store cookieShop = new Store("cookies", 12, 3.75);
      System.out.println("Sells " + cookieShop.productType);
